7fb1da8ae89b956cb54b4ce31856d986c553bcb0
Dark7eaper <41603013+ryanflash66@users.noreply.github.com>
2023-10-27 01:46:41 -0400

    Code in Not tested yet

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from pathlib import Path
-from llama_index import download_loader, SimpleDirectoryReader, VectorStoreIndex, LLMPredictor, PromptHelper, ServiceContext, StorageContext, load_index_from_storage
+from llama_index.callbacks.base import CallbackManager
+from llama_index import download_loader, LLMPredictor, PromptHelper, ServiceContext, StorageContext, load_index_from_storage
 from langchain.chat_models import ChatOpenAI
 from llama_hub.file.unstructured.base import UnstructuredReader
 from IPython.display import Markdown, display
+import chainlit as cl
 import os
 import openai
 
@@ -12,23 +13,16 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 UnstructuredReader = download_loader('UnstructuredReader')
 
-def construct_index(directory_path):
-    # set maximum input size
-    max_input_size = 4096
-    # set number of output tokens
-    num_outputs = 2000
-    # set maximum chunk overlap
-    max_chunk_overlap = 0.2
-    # set chunk size limit
-    chunk_size = 600
 
-    # define prompt helper
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size)
+try:
+    # rebuild storage context
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    # load index
+    index = load_index_from_storage(storage_context)
+except:
+    from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
-    # define LLM
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(openai_api_key=openai.api_key, temperature=0.2, model_name="gpt-4", max_tokens=num_outputs))
-
-    dir_reader = SimpleDirectoryReader(directory_path, file_extractor={
+    dir_reader = SimpleDirectoryReader("./data", file_extractor={
       ".txt": UnstructuredReader(),
       ".docx": UnstructuredReader(),
       ".pptx": UnstructuredReader(),
@@ -38,17 +32,94 @@ def construct_index(directory_path):
       ".eml": UnstructuredReader(),
       ".html": UnstructuredReader(),
     })
+
     documents = dir_reader.load_data()
-
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-    index = VectorStoreIndex.from_documents(documents, service_context=service_context)
-
+    index = VectorStoreIndex.from_documents(documents)
     index.storage_context.persist()
 
-    return index
+
+@cl.on_chat_start
+async def factory():
+    llm_predictor = LLMPredictor(
+        llm=ChatOpenAI(
+            temperature=0,
+            model_name="gpt-4",
+            streaming=True,
+        ),
+    )
+    service_context = ServiceContext.from_defaults(
+        llm_predictor=llm_predictor,
+        chunk_size=512,
+        callback_manager=CallbackManager([cl.LlamaIndexCallbackHandler()]),
+    )
+
+    query_engine = index.as_query_engine(
+        service_context=service_context,
+        streaming=True,
+    )
+
+    cl.user_session.set("query_engine", query_engine)
 
 
-def ask_jarvis():
+@cl.on_message
+async def main(message: cl.Message):
+    query_engine = cl.user_session.get("query_engine")  # type: RetrieverQueryEngine
+    response = await cl.make_async(query_engine.query)(message.content)
+
+    response_message = cl.Message(content="")
+
+    for token in response.response_gen:
+        await response_message.stream_token(token=token)
+
+    if response.response_txt:
+        response_message.content = response.response_txt
+
+    await response_message.send()
+
+
+
+
+
+############################################ OLD CODE ############################################
+# def construct_index(directory_path):
+#     # set maximum input size
+#     max_input_size = 4096
+#     # set number of output tokens
+#     num_outputs = 2000
+#     # set maximum chunk overlap
+#     max_chunk_overlap = 0.2
+#     # set chunk size limit
+#     chunk_size = 600
+
+#     # define prompt helper
+#     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size)
+
+#     # define LLM
+#     llm_predictor = LLMPredictor(llm=ChatOpenAI(openai_api_key=openai.api_key, temperature=0.2, model_name="gpt-4", max_tokens=num_outputs))
+
+#     dir_reader = SimpleDirectoryReader(directory_path, recursive=True, filename_as_id=True, file_extractor={
+#       ".txt": UnstructuredReader(),
+#       ".docx": UnstructuredReader(),
+#       ".pptx": UnstructuredReader(),
+#       ".pdf": UnstructuredReader(),
+#       ".jpg": UnstructuredReader(),
+#       ".png": UnstructuredReader(),
+#       ".eml": UnstructuredReader(),
+#       ".html": UnstructuredReader(),
+#     })
+#     documents = dir_reader.load_data()
+#     print(f"Loaded {len(documents)} docs!")
+
+
+#     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+#     index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+
+#     index.storage_context.persist()
+
+#     return index
+
+
+# def ask_jarvis():
     # rebuild storage context
     storage_context = StorageContext.from_defaults(persist_dir="./storage")
     # load index
@@ -61,5 +132,5 @@ def ask_jarvis():
 
 
 
-construct_index("data")
-ask_jarvis()
+# construct_index("data")
+# ask_jarvis()
